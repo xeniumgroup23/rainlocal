07d032b2723e19c8f9bac0fe8a63f00b3178bdee
xeniumgroup23 <62593021+xeniumgroup23@users.noreply.github.com>
2023-10-24 13:13:02 +0545

    home page

---
+++ rainlocal/js/all.js
@@ -0,0 +1,20 @@
+//Scroll Top Starts
+var mybutton = document.getElementById("scroll-top"); //Get the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+//Scroll Top Ends
+//
